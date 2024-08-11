daa9a3134ceca5b76f3efe80869cd2fed5a6dca1
allanctan <allanctan@gmail.com>
2024-08-11 22:40:30 +0800

    fixed labels

--- website/app/analyze/page.tsx
+++ website/app/analyze/page.tsx
@@ -125,22 +125,22 @@ export default function AnalyzeUser() {
       </h1>
       <div className="flow-root">
         <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8 ">
-          <div className="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
+          <div className="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8 overflow-x-auto ">
             {similarUsers.length > 0 ? (
               <table className="min-w-full divide-y divide-gray-300 border">
                 <thead>
                   <tr>
                     <th
                       scope="col"
-                      className="whitespace-nowrap p-2 border text-left text-sm font-semibold text-gray-900 sm:pl-0"
+                      className="whitespace-nowrap p-2 border text-center text-sm font-semibold text-gray-900 sm:pl-0"
                     >
-                      Similar User
+                      Name
                     </th>
                     <th
                       scope="col"
                       className="whitespace-nowrap p-2 border text-left text-sm font-semibold text-gray-900 sm:pl-0"
                     >
-                      Similarity
+                      Score
                     </th>
                     {Object.keys(similarUsers[0].swipes).map((key) => (
                       <th
